226457546cb8906b94e2d022f407974ea553eb7e
Tim Nelson <tim@eltimn.com>
2024-01-24 15:59:18 -0600

    Added /dom route to show it working with fromDomElement

--- src/main/scala/eltimn/ziohttpstring/MainApp.scala
+++ src/main/scala/eltimn/ziohttpstring/MainApp.scala
@@ -2,12 +2,13 @@ package eltimn.ziohttpstring
 
 import zio._
 import zio.http._
-import zio.http.template.Html
+import zio.http.template.{Dom, Html}
 
 object MainApp extends ZIOAppDefault {
 
   val routes = Routes(
-    Method.GET / "" -> handler(Response.html(Html.fromString("""<div class="container1 container2"><a href="http://zio.dev">ZIO Homepage</a></div>""")))
+    Method.GET / "" -> handler(Response.html(Html.fromString("""<div class="container1 container2"><a href="http://zio.dev">ZIO Homepage</a></div>"""))),
+    Method.GET / "dom" -> handler(Response.html(Html.fromDomElement(Dom.raw("""<div class="container1 container2"><a href="http://zio.dev">ZIO Homepage</a></div>""")))),
   )
 
   val app = routes.toHttpApp
